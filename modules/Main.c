bf886bc93ec2b3259ccd746946713c7d31286d1c
EmersonJSC <junior.emerson5@live.com>
2022-11-15 17:16:00 -0300

    Hash

---
+++ modules/Main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../modules/hash.h"
+
+
+
+int main(void){
+   
+   Palavra table[TAM];
+   
+   iniciartTable(table);
+   inserir(table);
+   // inserir(table,"susuro");
+     
+}
+
